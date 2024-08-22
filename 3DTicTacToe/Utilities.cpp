7effde9522cbb2e1fc2b7cf98a5c8a94c52f0ccb
MichalSzubinski <mich999.pl@gmail.com>
2024-08-22 15:51:35 +0200

    Fixed a bug that caused the game to end after two moves

--- 3DTicTacToe/Utilities.cpp
+++ 3DTicTacToe/Utilities.cpp
@@ -2,25 +2,40 @@
 
 void Utilities::PrintConsoleBoard(std::shared_ptr<Map> argMap)
 { 
+    // Y level splitter
+    std::string yLvlSplitter = "---";
+    for (int i = 1; i <= argMap->_xDimensions - 1; ++i)
+        yLvlSplitter = "---|" + yLvlSplitter;
+
+
     char tmpChar = ' ';
     for (int k = 1; k <= argMap->_zDimensions; ++k)
     {
-        std::cout << "Z = " << k << std::endl;
+        std::cout << "Z = " << k ;
+        for (int j = 1; j <= argMap->_yDimensions; ++j)
+            std::cout << "   ";
+        std::cout << "X\n"; // Upper right corner X
+
         // Printing for each level
         for (int i = 1; i <= argMap->_xDimensions; ++i)
         {
             for (int j = 1; j <= argMap->_yDimensions; ++j)
             {
+                if(j==1)std::cout << "  "; // Shift the board two spaces to the left
                 if (argMap->CheckIfPositionExists(Position(i, j, k))) tmpChar = argMap->_symbols[Position(i, j, k)]._symbol;
-                else tmpChar = 0x20;
+                else tmpChar = 0x20; // Space symbol
 
                 std::cout << " " << tmpChar << " ";
                 if (j < argMap->_yDimensions) std::cout << "|";
+                else std::cout << " " << i; // Show X at the end of the line
             }
             std::cout << std::endl;
-                if (i < argMap->_xDimensions) std::cout << "---|---|---" << std::endl;
+                if (i < argMap->_xDimensions) std::cout << "  "<< yLvlSplitter <<"    " << std::endl;
         }
-        std::cout << std::endl;
+        std::cout << std::endl << "Y "; // Lower left corner Y
+        for (int j = 1; j <= argMap->_yDimensions; ++j)
+            std::cout << " " << j << "  "; // Show Y at the bottom
+        if (k < argMap->_zDimensions) std::cout << "\n######################\n"; // End of the Z
     }
 }
 
